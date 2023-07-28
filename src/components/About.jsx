f2897fa853c6680dab6b66b147978d4198f22a01
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2023-07-28 15:38:21 +0300

    Finish Project

---
+++ src/components/About.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div name="about" className="w-full my-32">
+      <div className="max-w-[1240px] mx-auto">
+        <div className="text-center">
+          <h2 className="font-bold text-5xl">
+            Trusted by developers across the world
+          </h2>
+          <p className="text-3xl py-6 text-slate-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+            eius alias id fuga libero quos blanditiis laudantium
+          </p>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-1 px-2 text-center">
+          <div className="border py-8 border-slate-200 rounded-xl mx-2 shadow-xl">
+            <p className="text-6xl font-bold text-indigo-600">100%</p>
+            <p className="text-gray-400 mt-2">Completion</p>
+          </div>
+          <div className="border py-8 border-slate-200 rounded-xl mx-2 shadow-xl">
+            <p className="text-6xl font-bold text-indigo-600">24/7</p>
+            <p className="text-gray-400 mt-2">Delivery</p>
+          </div>
+          <div className="border py-8 border-slate-200 rounded-xl mx-2 shadow-xl">
+            <p className="text-6xl font-bold text-indigo-600">100k</p>
+            <p className="text-gray-400 mt-2">Transactions</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { Link, animateScroll as scroll } from "react-scroll";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+  return (
+    <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
+      <div className="px-2 flex justify-between items-center w-full h-full">
+        <div className="flex items-center ">
+          <h1 className="text-3xl font-bold mr-4 sm:text-4xl">BRAND.</h1>
+          <ul className="hidden md:flex">
+            <li className="cursor-pointer">
+              <Link to="home" smooth={true} duration={500}>
+                Home
+              </Link>
+            </li>
+            <li className="cursor-pointer">
+              <Link to="about" smooth={true} offset={-200} duration={500}>
+                About
+              </Link>
+            </li>
+            <li className="cursor-pointer">
+              <Link to="support" smooth={true} offset={-50} duration={500}>
+                Support
+              </Link>
+            </li>
+            <li className="cursor-pointer">
+              <Link to="platform" smooth={true} offset={-100} duration={500}>
+                Platform
+              </Link>
+            </li>
+            <li className="cursor-pointer">
+              <Link to="pricing" smooth={true} offset={-50} duration={500}>
+                Pricing
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="hidden md:flex">
+          <button className="bg-transparent border-transparent text-black mr-4">
+            Sign In
+          </button>
+          <button className="px-8 py-3">Sign Up</button>
+        </div>
+
+        <div className="md:hidden mr-4" onClick={handleClick}>
+          {!nav ? <MenuIcon className="w-5" /> : <XIcon className="w-5" />}
+        </div>
+      </div>
+      <div
+        className={
+          !nav
+            ? "hidden"
+            : "absolute bg-zinc-200 w-full px-8 ease-in-out duration-500"
+        }
+      >
+        <ul>
+          <li className="cursor-pointer border-b-2 border-zinc-300 w-full">
+            <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li className="cursor-pointer border-b-2 border-zinc-300 w-full">
+            <Link
+              onClick={handleClick}
+              to="about"
+              smooth={true}
+              offset={-200}
+              duration={500}
+            >
+              About
+            </Link>
+          </li>
+          <li className="cursor-pointer border-b-2 border-zinc-300 w-full">
+            <Link
+              onClick={handleClick}
+              to="support"
+              smooth={true}
+              offset={-50}
+              duration={500}
+            >
+              Support
+            </Link>
+          </li>
+          <li className="cursor-pointer border-b-2 border-zinc-300 w-full">
+            <Link
+              onClick={handleClick}
+              to="platform"
+              smooth={true}
+              offset={-100}
+              duration={500}
+            >
+              Platform
+            </Link>
+          </li>
+          <li className="cursor-pointer border-b-2 border-zinc-300 w-full">
+            <Link
+              onClick={handleClick}
+              to="pricing"
+              smooth={true}
+              offset={-50}
+              duration={500}
+            >
+              Pricing
+            </Link>
+          </li>
+          <div className=" flex flex-col my-4">
+            <button className="bg-transparent border-transparent text-black px-8 py-3 mb-4">
+              Sign In
+            </button>
+            <button className="px-8 py-3">Sing Up</button>
+          </div>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
